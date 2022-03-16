4e76589ffc57cc31512594d989127a26a85c9732
WassimSlim21 <Wassimslim18@gmail.com>
2022-03-16 21:00:17 +0100

    aa

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ http.createServer( function (request, response) {
       // Send the response body 
       response.end();
    });   
-}).listen(8082);
+}).listen(3000);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8082/');
+console.log('Server running at http://127.0.0.1:3000/');
